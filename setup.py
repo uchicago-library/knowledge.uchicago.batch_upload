99ab4f02688f581cc42acbf2714569b693243b6a
Tyler Danstrom <tdanstrom@uchicago.edu>
2017-08-15 14:43:15 -0500

    added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,22 @@ def _readme():
         return read_file.read()
 
 setup(
-    name="safgeneration",
-    description="A command-line script to generate Simple Archive Format directory for ingesting Proquest disseratations to a DSpace repository",
+    name="generating_safs_from_proquest",
+    author="Tyler Danstrom",
+    author_email="tdanstrom@uchicago.edu",
+    version="1.0.0",
+    license="LGPL3.0",
+    description="A command-line script to generate Simple Archive Format directory for " +\
+                "ingesting Proquest disseratations to a DSpace repository",
     long_description=_readme(),
+    keywords="python3.5 SimpleArchiveFormat generation csv proquest",
     packages = ['safgeneration'],
     scripts = ['bin/saf-generation.py'],
+    classifiers = [
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Education",
+        "Operating System :: POSIX :: Linux",
+        "Topic :: Text Processing :: Markup :: XML",
+    ]
 )
