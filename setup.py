31d06e4801f177f5a1b2d2121bbc4e0cc727d084
Tyler Danstrom <tyler.danstrom@gmail.com>
2017-12-05 11:37:01 -0600

    org'd setup.py to install scripts as executables

--- setup.py
+++ setup.py
@@ -1,26 +1,34 @@
-from setuptools import setup
+from os.path import exists
+from setuptools import setup, find_packages
 
 def _readme():
-    with open("README.md", "r") as read_file:
-        return read_file.read()
+    if exists("readme.md"):
+        with open("README.md", "r", encoding="utf-8") as read_file:
+            return read_file.read()
+    else:
+        return "no long description written"
 
 setup(
-    name="generating_safs_from_proquest",
+    name="safGeneration",
     author="Tyler Danstrom",
     author_email="tdanstrom@uchicago.edu",
     version="2.0.0",
-    license="LGPL3.0",
-    description="A command-line script to generate Simple Archive Format directory for " +\
-                "ingesting Proquest disseratations to a DSpace repository",
+    description="A set of tools for UChicago librarians to use to add material to knowledge.uchicago.edu",
     long_description=_readme(),
     keywords="python3.5 SimpleArchiveFormat generation csv proquest",
-    packages=['safgeneration'],
-    scripts=['bin/saf-generation.py'],
+    packages=find_packages(),
+    entry_points={
+         'console_scripts': [
+             'find_mamluk_files = safgeneration.findingMamlukFiles.__main__:main',
+             'generate_safs = safgeneration.generatingSAFS.__main__:main'
+         ]
+    },
+    license="LGPL3.0",
     classifiers=[
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Education",
-        "Operating System :: POSIX :: Linux",
-        "Topic :: Text Processing :: Markup :: XML",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
     ]
 )
